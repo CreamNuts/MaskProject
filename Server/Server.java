15aecfc0c4f4b9e00a0faf2a395b583e112c267f
SIEUN WOO <50469745+WOOSIEUN@users.noreply.github.com>
2020-11-25 05:41:42 +0900

    feat: Recv and send img obj for Server.

--- Server/Server.java
+++ Server/Server.java
@@ -1,47 +1,60 @@
+import java.awt.Image;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
  
 public class Server implements Runnable { 
-    public static final int ServerPort = 9898;
+    public static final int ServerPORT = 9898;
     public static final String ServerIP = "155.230.93.237";
+    public ServerSocket serverSocket;
+    public Socket csock;
+    public Image recvImg, sendImg;
  
-    @Override
-    public void run() {
- 
-        try {
-            System.out.println("Connecting...");
-            ServerSocket serverSocket = new ServerSocket(ServerPort);
- 
-            while (true) {
-                Socket client = serverSocket.accept();
-                System.out.println("Receiving...");
-                try {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                    String str = in.readLine();
-                    System.out.println("Received: '" + str + "'");                     
-                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
-                    out.println("Server Received " + str);
-                } catch (Exception e) {
-                    System.out.println("Error");
-                    e.printStackTrace();
-                } finally {
-                    client.close();
-                    System.out.println("Done.");
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Error");
-            e.printStackTrace();
-        }
+   public static void main(String[] args) { 
+        Thread ServerThread = new Thread(new Server());
+        ServerThread.start(); 
     }
- 
-    public static void main(String[] args) { 
-        Thread desktopServerThread = new Thread(new Server());
-        desktopServerThread.start(); 
-    } 
+   
+   @Override
+   public void run() {	
+       try {
+           System.out.println("Connecting...");
+           ServerSocket serverSocket = new ServerSocket(ServerPORT); 
+           while (true) {
+               Socket cilentSocket = serverSocket.accept();
+               System.out.println("Connect!");
+               try {
+                   //이미지 객체 수신
+               	ObjectInputStream objInputStream = new ObjectInputStream(cilentSocket.getInputStream());
+               	recvImg = (Image)objInputStream.readObject();
+                   System.out.println("S: Received: 'Image'");
+                   objInputStream.close();
+                   
+                   //파이썬 실행 코드 추가 필요
+                   //파이썬 코드 실행 종료시 대기하고 있다가 recvImg에 ImageIO.read(new File(<image file>));
+                   //패스 설정해서 객체 생성 해야함.
+                   
+                   //이미지 객체 전송
+                   ObjectOutputStream objOutStream = new ObjectOutputStream(cilentSocket.getOutputStream());
+                   objOutStream.writeObject(sendImg);
+                   objOutStream.close();
+               } catch (Exception e) {
+                   System.out.println("Error");
+                   e.printStackTrace();
+               } finally {
+                   cilentSocket.close();
+                   System.out.println("Done");
+               }
+           }
+       } catch (Exception e) {
+           System.out.println("Error");
+           e.printStackTrace();
+       }
+   } 
 }
