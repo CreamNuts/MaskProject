a674c7a4cf231f59abe061c4e1217d5e329b240c
WOOSIEUN <ramtk6726@naver.com>
2020-11-25 22:40:50 +0900

    feat: feat: Recv and send img for Android client.

--- AndroidClient/app/src/main/java/com/example/androidclient/MainActivity.java
+++ AndroidClient/app/src/main/java/com/example/androidclient/MainActivity.java
@@ -1,27 +1,33 @@
 package com.example.androidclient;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
-    private Socket sendSocket;
-    private BufferedReader networkReader;
-    private BufferedWriter networkWriter;
-    EditText sendText;
-    Button sendBtn;
+    private Socket socket;
+    private ObjectInputStream objInputStream;
+    private ObjectOutputStream objOutStream;
+    Image sendImg, recvImg;
+    ImageView sendImgView, recvImgView;
+    Button imgBtn, sendBtn;
+    private byte[] byteArray;
 
     private String IP = "155.230.93.237";
     private int PORT = 9898;
@@ -30,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sendText = (EditText) findViewById(R.id.sendText);
+        imgBtn = (Button) findViewById(R.id.imgBtn);
         sendBtn = (Button) findViewById(R.id.sendBtn);
         ConnectThread connectThread = new ConnectThread();
         connectThread.start();
@@ -40,24 +46,75 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         try {
-            sendSocket.close();
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void SendMessage(View view) {
-        if (sendText.getText().toString() != null || !sendText.getText().toString().equals("")) {
-            new Thread() {
-                public void run() {
-                    PrintWriter printWriter = new PrintWriter(networkWriter, true);
-                    String sendMsg = sendText.getText().toString();
-                    printWriter.println(sendMsg);
+    //이미지 불러오기
+    public void setImg(View view) {
+        Intent imgIntent = new Intent();
+        imgIntent.setType("image/*");
+        imgIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(imgIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream inStream = getContentResolver().openInputStream(data.getData());
+                    Bitmap sendImgBitmap = BitmapFactory.decodeStream(inStream);
+                    inStream.close();
+                    sendImgView.setImageBitmap(sendImgBitmap);
+
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    sendImgBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                    byteArray = byteArrayOutputStream.toByteArray();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            }.start();
+            }
         }
     }
 
+    //이미지 보내기
+    public void SendMessage(View view) {
+        new Thread() {
+            public void run() {
+                //이미지 전송
+                try {
+                    objOutStream = new ObjectOutputStream(socket.getOutputStream());
+                    objOutStream.writeObject(byteArray);
+                    objOutStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                recvImgThread.start();
+            }
+        }.start();
+    }
+
+    //이미지 받기
+    private Thread recvImgThread = new Thread() {
+
+        public void run() {
+            try {
+                objInputStream = new ObjectInputStream(socket.getInputStream());
+                byte[] inputData = (byte[]) objInputStream.readObject();
+                objInputStream.close();
+                Bitmap recvImgBitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+                recvImgView.setImageBitmap(Bitmap.createScaledBitmap(recvImgBitmap, recvImgView.getWidth(), recvImgView.getHeight(), false));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    };
+
+    //소켓 연결
     class ConnectThread extends Thread {
 
         public ConnectThread() {
@@ -65,37 +122,19 @@ public class MainActivity extends AppCompatActivity {
 
         public void run() {
             try {
-                //소켓
-                sendSocket = new Socket(IP, PORT);
-                //Writer Reader 설정
-                networkWriter = new BufferedWriter(new OutputStreamWriter(sendSocket.getOutputStream()));
-                networkReader = new BufferedReader(new InputStreamReader(sendSocket.getInputStream()));
-
+                //소켓 설정
+                socket = new Socket(IP, PORT);
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_LONG).show();
                     }
                 });
-
-                StartChat.start();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
     }
 
-    private Thread StartChat = new Thread() {
 
-        public void run() {
-            try {
-                String SendMessage;
-                while (true) {
-                    SendMessage = networkReader.readLine();
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-    };
 }
